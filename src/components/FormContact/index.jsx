2641846c6adf2445b4e38b96bff5272f74a05b1a
DebsLorena <105798685+DebsLorena@users.noreply.github.com>
2023-01-31 09:03:59 -0300

    Update index.jsx

--- src/components/FormContact/index.jsx
+++ src/components/FormContact/index.jsx
@@ -17,6 +17,7 @@ const FormContact = () => {
     const validationSchema = yup
         .object({
             name: yup.string().required(REQUIRED_VALIDATION("Name")).min(3, "Mínimo 3 caracteres.").matches(/^[A-z\s]+$/, "Inválido, apenas letras."),
+            adress: yup.string().required(REQUIRED_VALIDATION("Adress")).min(10, "Mínimo 10 caracteres.").matches(/^[A-z\s][0-9]+$/, "Endereço, inválido."),
             phone: yup.string().required(REQUIRED_VALIDATION("Phone")).min(11, "Mínimo 11 caracteres, inclusa o seu DDD.").matches(/^[0-9]+$/, "Apenas números."),
             date: yup.date("Inválido.").required(REQUIRED_VALIDATION("Date")),
             email: yup.string().email("Email inválido.").required(REQUIRED_VALIDATION("Email")).min(5, "Inválido"),
@@ -55,6 +56,21 @@ const FormContact = () => {
                         </Form.Text>
                     )}
                 </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formAdress">
+                    <Form.Label column="lg" lg={2}>Endereço</Form.Label>
+                    <Form.Control
+                        type="adress"
+                        placeholder="Insira o seu endereço"
+                        {...register("adress")}
+                    />
+                    {errors.adress && (
+                        <Form.Text className="text-danger">
+                            {errors.adress?.message}
+                        </Form.Text>
+                    )}
+                </Form.Group>
+
 
                 <Form.Group className="mb-3" controlId="formPhone">
                     <Form.Label column="lg" lg={2}>Telefone</Form.Label>
