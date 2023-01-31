ca7003066d889c3e6ccd741351e0ef8c3866d273
DebsLorena <105798685+DebsLorena@users.noreply.github.com>
2023-01-31 08:38:50 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import FormContact from './components/FormContact';
-import FormTest from './components/FormTest';
 
 function App() {
   return (
     <div className="App">
-      {/* <FormTest /> */}
       <FormContact />
     </div>
   );
